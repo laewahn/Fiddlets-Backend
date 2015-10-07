85367b04a4017e79f133208214d4ec561b3207fe
Dennis Lewandowski <dennis.lewandowski@rwth-aachen.de>
2015-10-07 12:10:40 +0200

    Adds a domain for the line info

---
+++ LineInfoDomain.js
@@ -0,0 +1,36 @@
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
+/*global require, exports */
+
+(function() {
+	"use strict";
+
+	var LINEINFO_DOMAIN = "EsprimaDomain";
+	var LINEINFO_VERSION = {major: 0, minor: 1};
+
+	var lineInfo = require("./LineInfo");
+
+	exports.init = function(domainManager) {
+		if (!domainManager.hasDomain(LINEINFO_DOMAIN)) {
+			domainManager.registerDomain(LINEINFO_DOMAIN, LINEINFO_VERSION);
+		}
+
+		domainManager.registerCommand(
+			LINEINFO_DOMAIN,
+			"infoForLine",
+			lineInfo.infoForLine,
+			false,
+			"Parses the given code",
+			[{
+				name: "line",
+				type: "string",
+				description: "The line"
+			}],
+			[{
+				name: "result",
+				type: "object",
+				description: "The line info for the line."
+			}]
+		);
+	};
+
+})();
